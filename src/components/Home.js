a05dc4afbd1cc92173366677e4ff7f8e3a5a1b8a
Ivan <i.poblete915@gmail.com>
2020-07-24 11:12:39 -0700

    created Home page

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+      <div>
+        <h1>
+          Ivan Poblete
+        </h1>
+        <p>full stack developer</p>
+      </div>
+  )
+}
